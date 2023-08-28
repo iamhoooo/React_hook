cac537aa117508a644c10de0d1fbc695ea4bff0b
SanghooPark <126745119+iamhoooo@users.noreply.github.com>
2023-08-28 12:01:22 +0900

    Add files via upload

---
+++ App.js
@@ -0,0 +1,37 @@
+import { useState } from 'react';
+
+const heavyWork = () => {
+  console.log('엄청 무거운 작업!!!');
+  return['홍길동', '김민수'];
+}
+
+function App(){
+  const [names, setNames] = useState(() => {
+    return heavyWork();
+  });
+  const [input, setInput] = useState('');
+
+  const handleInputChange = (e) => {
+    setInput(e.target.value);
+  };
+
+  const handleUpload = () => {
+    setNames((prevState) => {
+      return([input, ...prevState])
+    });
+  }
+
+  console.log(input);
+
+  return (
+    <div>
+     <input type="text" value={input} onChange={handleInputChange}></input>
+     <button onClick={handleUpload}>Upload</button>
+     {names.map((name, idx) => {
+      return <p key={idx}>{name}</p>
+     })}
+    </div>
+  );
+}
+
+export default App;
